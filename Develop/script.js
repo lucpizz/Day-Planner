f53709c6969bb66ddcaa0a55db01aeb26e665912
Luca Pizzoferrato <Luca.Pizzoferrato@Outlook.com>
2020-11-30 16:41:18 -0500

    updating index.html script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 
 
-$(document).ready(function ()
-{
+$(document).ready(function () {
 
     var today = moment().format("dddd, MMMM Do YYYY");
 
@@ -9,27 +8,34 @@ $(document).ready(function ()
 
     console.log(today);
 
-    
+
     function timeSlots() {
-        
-        var tm = moment().format('hh:mm a');
+
+        var tm = moment().format('hh:mm');
+
+        var saveBtn = $('<input>').attr({ class: '.saveBtn', type: 'submit', value: 'Save Event' });
+
         console.log(tm);
-        
-        for (tm = 0; tm <= 8; tm++){
+
+        for (tm = 0; tm <= 8; tm++) {
             var hour = tm + 8;
-            var timeSlotRow = $('<input/>').attr({ class: 'container', type: 'text', name: 'text', placeholder: 'Enter Event',time: `${[hour]}` }).addClass(".text");
-            $(".container").append(hour + ":00", timeSlotRow);
-            
-            
+            var timeSlotRow = $('<input/>').attr({ class: 'container', type: 'text', name: 'text', placeholder: 'Enter Event', time: `${[hour]}` }).addClass(".text");
+            $(".container").append(hour +":00", timeSlotRow, saveBtn);
+
+
             console.log(hour);
             console.log(timeSlotRow);
         }
-        
+
+
+
     }
 
-timeSlots();
 
-        
+
+    timeSlots();
+
+
 });
 
 
